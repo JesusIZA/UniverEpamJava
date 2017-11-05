520df658abe68156291fddcef6b0d14c662bb432
Jesus <deleterr1996@ukr.net>
2017-11-06 01:02:26 +0200

    Version 2.finished

--- HomeTask1(03.11.2017)/Task5.java
+++ HomeTask1(03.11.2017)/Task5.java
@@ -10,15 +10,86 @@ package epam.hw1;
  * @author Jesus Raichuk
  */
 public class Task5 {
+    /**
+     * Output items of square matrix by spiral
+     * @param A - matrix (must be int[][])
+     */
+    public static void OutBySpiral(int[][] A) {
+        /**
+         * Quantity of rows of the array will process
+         */
+        final int N = A.length;
+        /**
+         * Quantity of columns of the array will process
+         */
+        final int M = A[0].length;
+        /**
+         * If Array contain only one row or column
+         */
+        if(N == 1) {
+            Task1.printM(A[0]);
+        } else if (M == 1){
+            for (int i = 0; i < N; i++) {
+                System.out.print(A[i][0] + " ");
+            }
+        }
+        /**
+         * If array contain more than one row or column
+         */
+        else {
+            /**
+             * Quantity of items in the array A
+             */
+            int count = 0;
+
+            int i =0;
+            /**
+             * Output items by spiral
+             */
+            while(count < N * M) {
+                for (int k = i; k < M - i - 1; k++) {
+                    if(count == N * M) break;
+                    System.out.print(A[i][k]);
+                    count++;
+                }
+                for (int l = i; l < N - i - 1; l++) {
+                    if(count == N * M) break;
+                    System.out.print(A[l][M - i - 1]);
+                    count++;
+                }
+                for (int j = M - i - 1; j > i; j--) {
+                    if(count == N * M) break;
+                    System.out.print(A[N - i - 1][j]);
+                    count++;
+                }
+                for (int h = N - i - 1; h > i; h--) {
+                    if(count == N * M) break;
+                    System.out.print(A[h][i]);
+                    count++;
+                }
+                i++;
+                if(M % 2 == 1 && N % 2 == 1 && count == N * M - 1){
+                    count++;
+                    System.out.println(A[N/2][M/2]);
+                }
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         /**
-         * Size of array will process (N % 2 =1)
+         * Quantity of rows of the array will process (N % 2 =1)
          */
-        int n = 7;
+        final int N = 7;
+        /**
+         * Quantity of columns of the array will process (M % 2 =1)
+         */
+        final int M = 5;
         /**
          * Array will process
          */
-        int[][] A = new int[n][n];
+        int[][] A = new int[N][M];
         /*
          * Filling array A random values
          */
@@ -30,30 +101,7 @@ public class Task5 {
         
         System.out.println("A:");
         Task3.printM(A);
-
         System.out.println("By spiral:");
-        /**
-         * Output items by spiral
-         */
-        for (int i = 0; i < A.length; i++) {
-            for (int k = i; k < A[i].length - i - 1; k++) {
-                System.out.print(A[i][k]);
-            }
-            for (int l = i; l < A[i].length - i - 1; l++) {
-                System.out.print(A[l][A[i].length - i - 1]);
-            }
-            for (int j = A[i].length - i - 1; j > i; j--) {
-                System.out.print(A[A[i].length - i - 1][j]);
-            }
-            for (int h = A[i].length - i - 1; h > i; h--) {
-                System.out.print(A[h][i]);
-            }
-
-        }
-        /**
-         * if n % 2 == 1 output center element
-         */
-        if(n % 2 == 1) System.out.print(A[n/2][n/2]);
-
+        OutBySpiral(A);
     }
 }
